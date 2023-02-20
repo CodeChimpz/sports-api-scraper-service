9db65fd2a92065504fdb0aa4c1be2cd97f2fb145
taburet <md12072004@gmail.com>
2023-02-20 18:12:00 +0200

    jobs tested

---
+++ src/schema/Game.schema.ts
@@ -0,0 +1,18 @@
+import {Schema} from "mongoose";
+interface IGame {
+    schedule:
+        summary:"Los Angeles Rams @ Cincinnati Bengals"
+    details:
+        status:"final"
+    teams:
+        lastUpdated:"2022-02-14T03:02:18.640Z"
+    gameId:278943
+    odds:
+        venue:
+        scoreboard:score:
+        currentPeriod:4
+    periodTimeRemaining:"0:00"
+}
+const gameSchema = new Schema({
+
+})
